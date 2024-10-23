397d97420a6806f0d72d41916bb310f0e98fe4c1
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2024-10-23 11:48:30 -0600

    Practica 10
    Agregación de las rutas generales y de los usuarios

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
 // Ejemplo de activación de HOT RELOAD
-//console.log("Hola desde NodeJS, esto esta en hot reload")
-// because type:module, this not: const express = require('express'); // Importar la libreria para crear un servidor web - CommonJS
-// Instanciar nuestra aplicación
-const app = express(); 
-/* if type:module then
-*/
+// npm i -D nodemon
+// agregar a package.json 
+
+
 import express from 'express';
+import generalRoutes from './routes/generalRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
+const app = express(); 
+// COnfiguramos nuestro servidor web
 const port = 3000;
-
 app.listen(port, ()=>{
     console.log(`La aplicación ha iniciado en el puerto: ${port}`);
 });
 
 // Routing - Enrutamiento para peticiones
-
-app.get("/", function(req,res){
-    res.send("Hola desde el web en NodeJS")
-})
-
-app.get("/quieneres", function(req,res){
-    res.json(
-        {
-            "nombre":"Jesús Domínguez Ramírez",
-            "carrera":"TI DSM",
-            "grado":"4°",
-            "grupo":"A"
-        }
-    )
-});
+app.use('/',generalRoutes);
+app.use('/usuario',userRoutes);
 
 // 
